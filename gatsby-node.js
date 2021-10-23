1b50b6034f72632e2b31a09b81247946d96cc112
AbdulRahimFarooqui <67377097+AbdulRahimFarooqui@users.noreply.github.com>
2021-10-23 09:25:48 +0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
-exports.onCreatePages = async ({ page, actions }) => {
+exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions;
   if(page.path.match(/^\/lollies/)){
     page.matchPath = "/lollies/*";
