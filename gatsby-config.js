861b42aee01e244e76543672d5346e242bcb7a41
AbdulRahimFarooqui <67377097+AbdulRahimFarooqui@users.noreply.github.com>
2021-10-23 09:57:08 +0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,21 @@
-// In your gatsby-config.js
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
   plugins: [
-    // Simple config, passing URL
-    {
+     "gatsby-plugin-typescript",
+     {
       resolve: "gatsby-source-graphql",
       options: {
         // Arbitrary name for the remote schema Query type
-          typeName: "GETLOLLY",
+        typeName: "GETLOLLY",
         // Field under which the remote schema will be accessible. You'll use this in your Gatsby query
         fieldName: "getlolly",
         // Url to query from
         url: "https://ice-lolly-12-e.netlify.app/.netlify/functions/graphqlServerForLolly",
-      }
-    }
-  ]
+      },
+    },
+  ],
 }
