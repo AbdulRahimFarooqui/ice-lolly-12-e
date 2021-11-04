1842b244292f79dc369889f1d2ee7bda90ebdb05
AbdulRahimFarooqui <67377097+AbdulRahimFarooqui@users.noreply.github.com>
2021-11-04 11:13:43 +0500

    Update graphqlServerForLolly.js

--- functions/graphqlServerForLolly/graphqlServerForLolly.js
+++ functions/graphqlServerForLolly/graphqlServerForLolly.js
@@ -9,17 +9,17 @@ require('events').EventEmitter.defaultMaxListeners = 4000;
 const typeDefs = gql`
   type Query {
     hello: String
-    getlollies: [Lolly]
+    getlollies: [Lolly!]
     lollyByPath(link:String):Lolly
   }
   type Lolly {
-    reciepentName: String!
-    message: String!
-    sendersName:String!
-    colorTop: String!
-    colorMid: String!
-    colorBottom: String!
-    path: String!
+    reciepentName: String
+    message: String
+    sendersName:String
+    colorTop: String
+    colorMid: String
+    colorBottom: String
+    link: String
   }
 
   type Mutation{
@@ -63,27 +63,25 @@ const resolvers = {
     }
   },
   Mutation: {
-    createLolly: async (_, args) => {
+    createLolly: async (_, {reciepentName,message,sendersName,colorTop,colorMid,colorBottom}) => {
       const client = new faunadb.Client({ secret: 'fnAEPn8cZrACC_5HPUn5DbIjIAXsxndG3QZEniZL' })
-      const id = shortid.generate();
-      args.path = id;
 
       const result = await client.query(
         q.Create(q.Collection('lolly'), {
-          data: args
+          data: {reciepentName,message,sendersName,colorTop,colorMid,colorBottom,link:shortid.generate()}
         })
       );
 
       //call netlify BUILD_HOOK to start a new
-      await axios.post('https://api.netlify.com/build_hooks/5d46fa20da4a1b70047f2f04')
-      console.log("result: ", result);
+      //await axios.post('https://api.netlify.com/build_hooks/5d46fa20da4a1b70047f2f04')
+      //console.log("result: ", result);
       return {
         id: result.ref.id,
         c1: result.data.c1,
         c2: result.data.c2,
         c3: result.data.c3,
         sender: result.data.sender,
-        rec: result.data.rec,
+        rec: result.data.reciepentName,
         message: result.data.message,
         link: result.data.link
       }
